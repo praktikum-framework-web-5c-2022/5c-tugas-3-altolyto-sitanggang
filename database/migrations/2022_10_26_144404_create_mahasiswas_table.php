d6a62b6614784dfd4e51983775aca44f870644ca
Altolyto <altolyto@gmail.com>
2022-10-30 15:33:04 +0700

    mengumpulkan tugas3

---
+++ database/migrations/2022_10_26_144404_create_mahasiswas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('npm',13)->unique();
+            $table->string('nama',75);
+            $table->enum('jenis_kelamin',['Pria','Wanita']);
+            $table->text('alamat');
+            $table->string('tempat_lahir',75);
+            $table->date('tanggal_lahir');
+            $table->text('photo',75);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
